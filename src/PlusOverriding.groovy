0f5ad979a4d59b48a2955a910051809785f86709
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2016-09-02 14:24:16 -0400

    Overload plus operator

--- src/PlusOverriding.groovy
+++ src/PlusOverriding.groovy
@@ -12,8 +12,14 @@ class Money
         if(currency != other.currency) throw new IllegalArgumentException("Can't add $currency to $other.currency")
         return new Money(amount + other.amount, currency)
     }
+
+    Money plus(Integer more)
+    {
+        return new Money(amount + more, currency)
+    }
 }
 
 def buck = new Money(1, 'USD')
+
 assert buck + buck == new Money(2, 'USD')
-println((buck + buck).amount)
+assert buck + 4 == new Money(5, 'USD')
