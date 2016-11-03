953040036f08a7ce65cca1bb279aeb45232403d7
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2016-11-03 11:26:48 -0400

    Implement xml file reader

---
+++ src/main/groovy/XmlFileReader.groovy
@@ -0,0 +1,7 @@
+def parsedFile = new XmlSlurper().parse(new File('content.xml'))
+
+def categories = parsedFile.depthFirst().findAll {it.name() == 'category'}
+def result = new HashSet<>(categories.collect { "PREFIX $it" })
+
+def outputFile = new File('output.data')
+result.each {outputFile.append("$it\n")}
