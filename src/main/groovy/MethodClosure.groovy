f42750e4c0fe1a6932a9779365e3ad653de40616
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2016-11-16 09:09:28 -0500

    Add overloading in method closure

--- src/main/groovy/MethodClosure.groovy
+++ src/main/groovy/MethodClosure.groovy
@@ -6,6 +6,11 @@ class SizeFilter
     {
         return value.size() < limit
     }
+
+    boolean upTo(int value)
+    {
+        return value < limit
+    }
 }
 
 def filter5 = new SizeFilter(limit: 5)
@@ -14,6 +19,8 @@ def filter6 = new SizeFilter(limit: 6)
 Closure upTo5 = filter5.&upTo
 
 def words = ['small', 'tiny', 'very big']
+def digits = [1, 2, 3, 4, 5, 6, 7]
 
 assert 'tiny' == words.find(upTo5)
 assert 2 == words.findAll(filter6.&upTo).size()
+assert 4 == digits.findAll(upTo5).size()
